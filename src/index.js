b4d917e318fd6e0074568a203f6a7593bfe706bd
lkush <l.kushnariova@yandex.ru>
2018-02-24 20:46:27 +0300

    Task implementation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arr = []
+        this.comparator = (a, b) => a - b
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element)
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.arr.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arr;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort((a, b) => a - b);
+        let toSort = [];
+        for (let i = 0; i < indices.length; i++) {
+            toSort.push(this.arr[indices[i]])
+        }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        toSort.sort(this.comparator)
+
+        for (let i = 0; i < indices.length; i++) {
+            this.arr[indices[i]] = toSort[i]
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction
+    }
 }
 
 module.exports = Sorter;
